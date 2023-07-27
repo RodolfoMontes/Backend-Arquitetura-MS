70c9bcf24e9fba175a5e01076166bcd2ce2a868d
alpha7 <montes.rodolforodrigues@gmail.com>
2023-07-26 22:50:35 -0300

    Inicializando Repositório
    Tipo: M (C-Correção, I-Implementação, M-Melhoria)
    
    Descrição:
        - Inicializando Repositório
    Observações:
    
    Referências:

---
+++ config-server/src/test/java/com/meuexemplo/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meuexemplo.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
